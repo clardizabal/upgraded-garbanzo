100212190559ee7a0bf640c6b72d0be69828e293
clardizabal <chris.lardizabal@gmail.com>
2016-10-28 14:25:41 -0700

    use component directive for inputs

--- client/app.js
+++ client/app.js
@@ -1,3 +1,30 @@
 "use strict";
-angular.module('app', [
-]);
+angular.module("app", [])
+  .component('myInput', {
+    templateUrl: 'my_input.html',
+    controller: function($scope) {
+      $scope.index = this.index;
+      $scope.title = this.title;
+    },
+    bindings: {
+      index: '=',
+      title: '=',
+      value: '='    
+    }
+  }) 
+  // .directive('myInput', function() {
+  //   return {
+  //     scope: {
+  //       index: '=',
+  //       title: '=',
+  //       value: '='
+  //     },
+  //     templateUrl: 'my_input.html',
+  //     controller: 'InputCtrl'
+  //   }
+  // })
+  // .controller('InputCtrl', function($scope) {
+  //   $scope.index = this.index;
+  //   $scope.title = 'Current Age';
+  //   $scope.value = 28;
+  // })
